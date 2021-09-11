349ed13e85a37547c6332656bcb93b66d9633fde
jacobnewland <89615158+jacobnewland@users.noreply.github.com>
2021-09-11 11:23:34 -0500

    Copied Dr. Goadrich's dog code.  Let's see if that works here.

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
+// $(document).ready(function() {
+//   $("#randomizer-button").click(function() {
+//     $(document).ready(function() {
+//   $('#clicker').click(function() {
+//     $.ajax({
+//       dataType: "json",
+//       url: "https://random-d.uk/api",
+//       success: function(results) {
+//         console.log(results["url"]);
+//
+//           $('#duck').attr("src", results["url"]);
+//
+//       },
+//       error: function(xhr,status,error) {
+//         console.log(error);
+//       }
+//     });
+//   //   $.ajax({
+//   //     dataType: "jsonp",
+//   //     jsonpCallback: "parseQuote",
+//   //     url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
+//   //     success: function(results) {
+//   //       $('.quote').text(results["quoteText"]);
+//   //     },
+//   //     error: function(xhr,status,error) {
+//   //       console.log(error);
+//   //     }
+//   // });
+// });
+//   });
+//     });
+//
+// });
+
 $(document).ready(function() {
-  $("#randomizer-button").click(function() {
-    $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
       dataType: "json",
-      url: "https://random-d.uk/api",
+      url: "https://random.dog/woof.json",
       success: function(results) {
         console.log(results["url"]);
-
-          $('#duck').attr("src", results["url"]);
-
+        if (results["url"].endsWith(".mp4")) {
+          $('#dog').attr("src", "images/blank.png");
+        } else {
+          $('#dog').attr("src", results["url"]);
+        }
       },
       error: function(xhr,status,error) {
         console.log(error);
       }
     });
-  //   $.ajax({
-  //     dataType: "jsonp",
-  //     jsonpCallback: "parseQuote",
-  //     url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
-  //     success: function(results) {
-  //       $('.quote').text(results["quoteText"]);
-  //     },
-  //     error: function(xhr,status,error) {
-  //       console.log(error);
-  //     }
-  // });
-});
   });
-    });
-
 });
