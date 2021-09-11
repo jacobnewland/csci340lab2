9461b8be4d8703e9d68ad4521936965a864ba07f
jacobnewland <89615158+jacobnewland@users.noreply.github.com>
2021-09-11 18:43:39 -0500

    My button issue was actually a CORS error.  I think it's fixed now

--- script.js
+++ script.js
@@ -1,53 +1,30 @@
-// $(document).ready(function() {
-//   $("#randomizer-button").click(function() {
-//     $(document).ready(function() {
-//   $('#clicker').click(function() {
-//     $.ajax({
-//       dataType: "json",
-//       url: "https://random-d.uk/api",
-//       success: function(results) {
-//         console.log(results["url"]);
-//
-//           $('#duck').attr("src", results["url"]);
-//
-//       },
-//       error: function(xhr,status,error) {
-//         console.log(error);
-//       }
-//     });
-//   //   $.ajax({
-//   //     dataType: "jsonp",
-//   //     jsonpCallback: "parseQuote",
-//   //     url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
-//   //     success: function(results) {
-//   //       $('.quote').text(results["quoteText"]);
-//   //     },
-//   //     error: function(xhr,status,error) {
-//   //       console.log(error);
-//   //     }
-//   // });
-// });
-//   });
-//     });
-//
-// });
-
 $(document).ready(function() {
   $('#clicker').click(function() {
     $.ajax({
       dataType: "json",
-      url: "https://random-d.uk/api/random",
+      url: "https://cors-anywhere.herokuapp.com/https://random-d.uk/api/random",  //https://cors-anywhere.herokuapp.com/corsdemo
       success: function(results) {
-        console.log(results["url"]);
+        console.log(results["url"]);https://cors-anywhere.herokuapp.com/
         if (results["url"].endsWith(".mp4")) {
-          $('#duck').attr("src", "duckhunt.jpg");
+          $('#duck').attr("src", "duckhunt.png");
         } else {
-          $('#duck').attr("src", results["url"]);
+          $('#duck').attr("src", results["url"] );
         }
       },
       error: function(xhr,status,error) {
         console.log(error);
       }
+      $.ajax({
+            dataType: "json",
+            url: "https://cors-anywhere.herokuapp.com/corsdemo/https://breaking-bad-quotes.herokuapp.com/v1/quotes", //https://cors-anywhere.herokuapp.com/corsdemo
+            success: function(results) {
+              $('.quote').text(results["quoteText"]);
+            },
+            error: function(xhr,status,error) {
+              console.log(error);
+            }
+        });
+
     });
   });
 });
